62931231eacf7f715a1d5c57a68134e813e0f9af
edxh92 <ed_xh@hotmail.com>
2017-11-24 19:43:11 -0600

    version final

--- final/menu/views.py
+++ final/menu/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
 from .forms import MenuForm
 from menu.models import Menu
@@ -8,8 +8,8 @@ def menu_nuevo(request):
    if request.method == "POST":
       formulario = MenuForm(request.POST)
       if formulario.is_valid():
-         menu = Menu.objects.create(nombre=formulario.cleaned_data['nombre'],descripcion=formulario.cleaned_data['descripcion'], costo_puntada = formulario.cleaned_data['costo_puntada'],status =formulario.cleaned_data['status'])
+         menu = Menu.objects.create(nombre=formulario.cleaned_data['nombre'],descripcion=formulario.cleaned_data['descripcion'], costo_puntada = formulario.cleaned_data['costo_puntada'])
       return redirect('/')
    else:
-       formulario = PuntadaForm()
+       formulario = MenuForm()
    return render(request, 'menu_nuevo.html', {'formulario': formulario})
