199d537ed46b72c5c9211ab8e84f64385ada1638
edxh92 <ed_xh@hotmail.com>
2017-11-24 19:32:10 -0600

    Nuevo commit

--- final/menu/forms.py
+++ final/menu/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import Menu
 
 
-class Menu(forms.ModelForm):
+class MenuForm(forms.ModelForm):
     class Meta:
         model=Menu
         fields=('nombre','descripcion','costo_puntada')
--- final/menu/views.py
+++ final/menu/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.contrib import messages
+from .forms import MenuForm
+from menu.models import Menu
 # Create your views here.
+
+def menu_nuevo(request):
+   if request.method == "POST":
+      formulario = MenuForm(request.POST)
+      if formulario.is_valid():
+         menu = Menu.objects.create(nombre=formulario.cleaned_data['nombre'],descripcion=formulario.cleaned_data['descripcion'], costo_puntada = formulario.cleaned_data['costo_puntada'],status =formulario.cleaned_data['status'])
+      return redirect('/')
+   else:
+       formulario = PuntadaForm()
+   return render(request, 'menu_nuevo.html', {'formulario': formulario})
